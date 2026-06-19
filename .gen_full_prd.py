f1ef72b75b5485f71f7eb05ec52c9b87797cda26
ssdeanx <ssdeanx@gmail.com>
2026-06-19 10:48:16 -0400

    chore: remove unused generation scripts
    - Deleted multiple generation scripts related to project requirements and HUD elements:
      - .gen_clear.py
      - .gen_complete.py
      - .gen_full_prd.py
      - .gen_hud.py
      - .gen_hud2.py
      - .gen_more.py
      - .gen_more2.py
      - .gen_prd.py
      - .gen_prd_full.py
      - .gen_step1.py

--- .gen_full_prd.py
+++
@@ -1,6 +0,0 @@
-import os
-os.chdir("C:/Users/ssdsk/projects/SOCOM")
-print("Writing PRD...")
-with open("docs/Specs/requirements.md", "w", encoding="utf-8") as r:
-    r.write("# SOCOM PRD\n\nTest write successful\n")
-print("Done")
